c6110d1ccbd502f4c6550a7362ceb0e9c8fcadf7
analiacisneros <analiacisneros1394@gmail.com>
2025-05-26 20:04:41 -0300

    feat: funcion venderEntrada

--- src/packageTicketek/Ticketek.java
+++ src/packageTicketek/Ticketek.java
@@ -177,8 +177,8 @@ public class Ticketek implements ITicketek {
 			            	break;
 			            }
 			            
-			    	}//fin del for
-			   // }
+			    	}
+			   //
 			   
 		//}//fin for
 		

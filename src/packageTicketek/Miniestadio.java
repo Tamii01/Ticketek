7763fcb3d1017375e633b625b3de3f5715eac055
analiacisneros <analiacisneros1394@gmail.com>
2025-05-26 15:22:17 -0300

    feat: atributos y constructor

--- src/packageTicketek/Miniestadio.java
+++ src/packageTicketek/Miniestadio.java
@@ -2,15 +2,30 @@ package packageTicketek;
 
 public class Miniestadio extends Sede{
 	
+	int asientosPorFila;
+	int puestos;
 	double consumisionLibre;
-	int puestoMerchandasing;
-	int puestoComidaRapida;
+	String[] sectores;
+	int[] capPorSector;
+	int[] porcentajeAdicional;
 	
-	public Miniestadio(String nombre, String direccion, int capacidadMaxima, double consumisionLibre, int puestoMerchandasing, int puestoComidaRapida) {
+	
+	
+	
+	public Miniestadio(String nombre, String direccion, int capacidadMaxima, int asientosPorFila,int puestos, 
+			double consumision, String[] sectores, int[] capPorSector, int[] porcentajeAdicional) {
 		super(nombre, direccion, capacidadMaxima);
-		this.consumisionLibre = consumisionLibre;
-		this.puestoMerchandasing = puestoMerchandasing;
-		this.puestoComidaRapida = puestoComidaRapida;
+		this.asientosPorFila=asientosPorFila;
+		this.puestos=puestos;
+		this.consumisionLibre=consumision;
+		this.sectores= sectores;
+		this.porcentajeAdicional=porcentajeAdicional;
+		
 	}
+	
+	//si tiene asientos o sectores
+	public boolean esNumerada() {
+		   return true;
+		}
 
 }

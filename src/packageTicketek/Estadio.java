5cca07f1803d870a5c42674ccbf96963794a203c
analiacisneros <analiacisneros1394@gmail.com>
2025-06-14 15:17:01 -0300

    feat. atributos estadio

--- src/packageTicketek/Estadio.java
+++ src/packageTicketek/Estadio.java
@@ -2,20 +2,17 @@ package packageTicketek;
 
 public class Estadio extends Sede {
 
-	int precio;
+
 	
 	public Estadio(String nombre, String direccion, int capacidadMaxima) {
 		super(nombre, direccion, capacidadMaxima);
 		
 	}
 
-	@Override
 	public boolean esNumerada() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
-	public boolean esNumerada() {
-	   return false;
-	}
+	
 }
